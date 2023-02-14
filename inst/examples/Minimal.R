ff53c8a2bcbd998d014e76849de022fca314345e
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-02-14 18:40:38 +0000

    improve looks of minimal example

--- inst/examples/Minimal.R
+++ inst/examples/Minimal.R
@@ -4,12 +4,24 @@ library(shiny.molstar)
 
 pdbId <- "1LOL" # nolint: linter_name
 
-print(Molstar(pdbId = pdbId, dimensions = c(300, 300)))
-
 shinyApp(
   ui = tagList(
-    h1(glue("Molecular visualization \"{pdbId}\"")),
-    Molstar(pdbId = pdbId, dimensions = c(300, 300))
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+    tags$main(
+      h2(glue("Molecular visualization \"{pdbId}\"")),
+      Molstar(pdbId = pdbId, dimensions = c(300, 300))
+    ),
+    tags$footer(
+      tags$p(
+        "Minimal example from ",
+        tags$a(href = "https://appsilon.github.io/shiny.molstar", "shiny.molstar"),
+        "package"
+      ),
+      tags$p(
+        "Made with ❤️ at",
+        tags$a(href = "https://appsilon.github.io/shiny.molstar", "Appsilon")
+      )
+    )
   ),
   server = function(input, output) {
   }
