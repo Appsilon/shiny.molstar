b94b3bb05f79afbe59822547eac6d315bdb7a722
André Veríssimo <211358+averissimo@users.noreply.github.com>
2023-02-14 18:56:09 +0000

    adds full page editor

---
+++ inst/examples/FullPage.R
@@ -0,0 +1,17 @@
+library(glue)
+library(shiny)
+library(shiny.molstar)
+
+shinyApp(
+  ui = tagList(
+    tags$style(HTML("html, body { margin: 0; }")),
+    Molstar(
+      pdbId = "1LOL",
+      useInterface = TRUE,
+      showControls = TRUE,
+      showAxis = TRUE
+    )
+  ),
+  server = function(input, output) {
+  }
+)
--- inst/examples/Minimal.R
+++ inst/examples/Minimal.R
@@ -6,22 +6,34 @@ pdbId <- "1LOL" # nolint: linter_name
 
 shinyApp(
   ui = tagList(
-    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
     tags$main(
       h2(glue("Molecular visualization \"{pdbId}\"")),
-      Molstar(pdbId = pdbId, dimensions = c(300, 300))
+      Molstar(
+        pdbId = pdbId,
+        dimensions = c(300, 300),
+        showAxis = TRUE
+      )
     ),
+    #
+    # Footer and Styling
+    #
     tags$footer(
       tags$p(
         "Minimal example from ",
-        tags$a(href = "https://appsilon.github.io/shiny.molstar", "shiny.molstar"),
+        tags$a(
+          href = "https://appsilon.github.io/shiny.molstar",
+          "shiny.molstar"
+        ),
         "package"
       ),
       tags$p(
         "Developed with ❤️ by",
-        tags$a(href = "https://appsilon.github.io/shiny.molstar", "Appsilon")
+        tags$a(
+          href = "https://appsilon.github.io/shiny.molstar", "Appsilon"
+        )
       )
-    )
+    ),
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
   ),
   server = function(input, output) {
   }
